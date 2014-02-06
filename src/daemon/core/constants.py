799c6ff0b559beb749b1b4f56fc824fa76f6bf90
Robert Wuttke <robert@benocs.com>
2014-02-06 15:33:29 +0100

    [core/constants] using ipaddrs.conf it is now possible to configure ip-address ranges to be used
    Signed-off-by: Robert Wuttke <robert@benocs.com>

--- src/daemon/core/constants.py
+++ src/daemon/core/constants.py
@@ -17,3 +17,35 @@ VIMAGE_BIN              = "no/vimage"
 QUAGGA_STATE_DIR        = "/var/run/quagga"
 MOUNT_BIN               = "/bin/mount"
 UMOUNT_BIN              = "/bin/umount"
+
+
+config_files = ['ipaddrs.conf']
+CONFIGS = {}
+from os.path import exists, expanduser, join
+# since this is the core-daemon, user should always expand to /root/
+home = expanduser('~')
+dotcore = join(home, '.core')
+for configfile in config_files:
+    fullpath = join(dotcore, configfile)
+    configname = configfile[:configfile.rfind('.conf')]
+    if exists(fullpath):
+        #CONFIGS[configname] = {}
+        localconfig = {}
+        with open(fullpath, 'r') as f:
+            for line in f:
+                try:
+                    key_value = line.split('=')
+                    if not len(key_value) == 2:
+                        print('ERROR: could not read line: %s' % line)
+                        continue
+
+                    key = key_value[0].replace('"', '').replace("'", "").strip()
+                    value = key_value[1].replace('"', '').replace("'", "").strip()
+
+                    print('new config: %s, %s = %s' % (configname, key, value))
+                    localconfig[key] = value
+                except:
+                    print('ERROR: could not read line: %s' % line)
+        if len(localconfig) > 0:
+            CONFIGS[configname] = localconfig
+
