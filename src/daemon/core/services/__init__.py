537f4e329a3deaa35942308607a1abc20f85332a
Robert Wuttke <robert@benocs.com>
2014-02-06 15:37:46 +0100

    [services/__init__] enabled router_services
    Signed-off-by: Robert Wuttke <robert@benocs.com>

--- src/daemon/core/services/__init__.py
+++ src/daemon/core/services/__init__.py
@@ -4,4 +4,4 @@ Services available to nodes can be put in this directory.  Everything listed in
 __all__ is automatically loaded by the main core module.
 """
 __all__ = ["quagga", "nrl", "xorp", "bird", "utility", "security", "ucarp",
-           "monitoring", "service_flags"]
+           "monitoring", "service_flags", "router_services"]
