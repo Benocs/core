48967543678e12f8f6eec680b04cde9380a81f31
Robert Wuttke <robert@benocs.com>
2014-02-06 15:45:31 +0100

    [misc/ipaddr] removed obsolete ipaddress module (as we are now importing it directly from python); implemented class NetIDNodeMap() which computes loopback-addresses based on AS number and node id
    Signed-off-by: Robert Wuttke <robert@benocs.com>

--- src/daemon/core/misc/ipaddr.py
+++ src/daemon/core/misc/ipaddr.py
@@ -217,3 +217,21 @@ def isIPv4Address(addrstr):
 
 def isIPv6Address(addrstr):
     return isIPAddress(AF_INET6, addrstr)
+
+class NetIDNodeMap():
+    # key: netid (as int) - value: current node count of that netid.
+    # first node is assigned id 1
+    mapping = {}
+
+    @staticmethod
+    def register_node(nodeid, netid):
+        if not netid in NetIDNodeMap.mapping:
+            NetIDNodeMap.mapping[netid] = {}
+        if not nodeid in NetIDNodeMap.mapping[netid]:
+            if len(NetIDNodeMap.mapping[netid].values()) == 0:
+                NetIDNodeMap.mapping[netid][nodeid] = 1
+            else:
+                NetIDNodeMap.mapping[netid][nodeid] = max(NetIDNodeMap.mapping[netid].values()) + 1
+
+        return NetIDNodeMap.mapping[netid][nodeid]
+
