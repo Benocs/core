c846acf40e60a35b87907d74858f8909402eedb1
Robert Wuttke <robert@benocs.com>
2014-02-06 15:34:55 +0100

    [services/router_services] various CORE services that implement functions for routers
    Signed-off-by: Robert Wuttke <robert@benocs.com>

---
+++ src/daemon/core/services/router_services.py
@@ -0,0 +1,84 @@
+# various CORE services that implement functions for routers
+#
+# Copyright (C) 2014 Robert Wuttke <robert@benocs.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+import os
+
+from core.service import CoreService, addservice
+from core.misc.ipaddr import IPv4Prefix, IPv6Prefix
+from core.misc.utils import *
+
+from core.services import utility
+
+class LoopbackAddress(utility.UtilService):
+    """ assigns a loopback ip address to each router """
+    _name = "LoopbackAddress"
+    _configs = ("loopback.sh",)
+    _startup = ("sh loopback.sh",)
+    _meta = "assigns a loopback ip address to each router"
+
+    @classmethod
+    def generateconfig(cls, node, filename, services):
+        addr = node.getLoopbackIPv4()
+
+        cfg = ''.join([
+            "#!/bin/sh\n",
+            "ip addr add %s/32 dev lo\n" % addr
+            ])
+        return cfg
+
+addservice(LoopbackAddress)
+
+class InterASStaticRoute(utility.UtilService):
+    """ set up a static route per external interface to point to loopback """
+    _name = "InterASStaticRoute"
+    _configs = ("interasstaticroute.sh",)
+    _startup = ("sh interasstaticroute.sh",)
+    _meta = "set up a static route per external interface to point to loopback"
+
+    @classmethod
+    def generateconfig(cls, node, filename, services):
+        cfg = "#!/bin/sh\n"
+
+        # find any link on which two different netid's (i.e., AS numbers) are
+        # present and configure a bgp-session between the two corresponding nodes.
+        for localnetif in node.netifs():
+
+            # do not include control interfaces
+            if hasattr(localnetif, 'control') and localnetif.control == True:
+                continue
+
+            for idx, net_netif in localnetif.net._netif.items():
+                candidate_node = net_netif.node
+
+                # skip our own interface
+                if localnetif == net_netif.node:
+                    continue
+
+                # found two different ASes
+                if not node.netid == net_netif.node.netid:
+                    if (hasattr(node, 'type') and node.type == "egp_node") and \
+                            (hasattr(net_netif.node, 'type') and \
+                            net_netif.node.type == "egp_node"):
+                        remote_node_addr = net_netif.node.getLoopbackIPv4()
+                        for addr in net_netif.addrlist:
+                            addr = addr.split('/')[0]
+                            cfg += "  ip route add %s/32 via %s\n" % (str(remote_node_addr), str(addr))
+
+        return cfg
+
+addservice(InterASStaticRoute)
