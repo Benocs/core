c6ae68c4d36c3b73729db54fdc7c20a6729cb138
Robert Wuttke <robert@benocs.com>
2014-06-16 17:55:09 +0200

    [misc/netid] added class NetIDSubnetMap() - mapping between netids and subnets
    This class manages a mapping between known netids and their associated
    IP subnets. It takes a NetID as input and outputs the corresponding
    subnet-id. This id is then used by the ipaddr module to calculate
    subnet-prefixes from it.
    
    Signed-off-by: Robert Wuttke <robert@benocs.com>

--- src/daemon/core/misc/netid.py
+++ src/daemon/core/misc/netid.py
@@ -22,3 +22,43 @@ class NetIDNodeMap():
 
         return NetIDNodeMap.mapping[netid][nodeid]
 
+class NetIDSubnetMap():
+    """
+        This class manages a mapping between known netids and their associated
+        IP subnets. It takes a NetID as input and outputs the corresponding
+        subnet-id. This id is then used by the ipaddr module to calculate
+        subnet-prefixes from it.
+    """
+
+    __max_subnets_ipv4__ = 0x100
+    __max_subnets_ipv6__ = 0x10000
+
+    __mapping__ = {4:{}, 6:{}}
+
+    @staticmethod
+    def register_netid(netid, ipfam=4):
+        if not ipfam == 4 and not ipfam == 6:
+            raise ValueError("Invalid IP version: '%s'" % ipfam)
+
+        if ipfam == 4:
+            max_subnets = NetIDSubnetMap.__max_subnets_ipv4__
+        elif ipfam == 6:
+            max_subnets = NetIDSubnetMap.__max_subnets_ipv6__
+
+        net_candidate = netid % max_subnets
+
+
+        cnt = 0
+        while net_candidate in NetIDSubnetMap.__mapping__[ipfam] and \
+                not NetIDSubnetMap.__mapping__[ipfam][net_candidate] == netid:
+            net_candidate = ((net_candidate + 1) % max_subnets)
+
+            # failsafe switch in case no more subnets can be assigned
+            cnt += 1
+            if cnt > max_subnets:
+                raise ValueError('There are no more free subnets available')
+
+        # if we reach this, then no errors were encountered
+        NetIDSubnetMap.__mapping__[ipfam][net_candidate] = netid
+
+        return net_candidate
