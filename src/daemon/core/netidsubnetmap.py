d26c06624c0540d060cac0a03ca08179990103e7
Robert Wuttke <robert@benocs.com>
2014-06-20 14:32:14 +0200

    [NetIDSubnetMapManager] manages/brokers netidsubnetmap to gui, etc
    Signed-off-by: Robert Wuttke <robert@benocs.com>

---
+++ src/daemon/core/netidsubnetmap.py
@@ -0,0 +1,113 @@
+# Copyright (C) 2014 Benocs GmbH
+#
+# author: Robert Wuttke <robert@benocs.com>
+#
+# See the LICENSE file included in this distribution.
+#
+
+from core.api import coreapi
+from core.conf import ConfigurableManager, Configurable
+from core.misc.netid import NetIDSubnetMap
+
+class NetIDSubnetMapManager(ConfigurableManager):
+    _name = "NetIDSubnetMapManager"
+    _type = coreapi.CORE_TLV_REG_UTILITY
+
+    def __init__(self, session):
+        super().__init__(session)
+        self.verbose = self.session.getcfgitembool('verbose', False)
+        self.register()
+
+    def register(self):
+        """ Register models as configurable object(s) with the Session object.
+        """
+        models = [NetIDSubnetMapIPv4, NetIDSubnetMapIPv6]
+        for m in models:
+            self.session.addconfobj(m._name, m._type, m.cfg_callback)
+
+    @classmethod
+    def toconfmsg(cls, flags, nodenum, typeflags, values):
+        raise NotImplementedError("use toconfmsgs() instead")
+
+    @classmethod
+    def toconfmsgs(cls, flags=None, nodenum=None, typeflags=None, values=None):
+        msgs = []
+
+        if flags is None:
+            flags = 0
+        if typeflags is None:
+            typeflags = coreapi.CONF_TYPE_FLAGS_UPDATE
+
+        models = [NetIDSubnetMapIPv4, NetIDSubnetMapIPv6]
+        for m in models:
+            msgs.append(m.toconfmsg(flags, nodenum, typeflags, values,
+                    cls.session.sessionid))
+        return msgs
+
+class BaseNetIDSubnetMap(Configurable):
+
+    _type = coreapi.CORE_TLV_REG_UTILITY
+    _bitmap = None
+    _name = None
+    __ipversion__ = None
+    __data_type__ = coreapi.CONF_DATA_TYPE_UINT32
+
+    def __init__(self, session, objid, verbose=False, values=None):
+        super().__init__(self, session, objid)
+        self.verbose = verbose
+
+        print(('[%s] values: %s' % (self._name, str(values))))
+
+    @classmethod
+    def cfg_callback(cls, session, msg):
+        print(('[%s] msg: %s' % (cls._name, str(msg))))
+        values_str = msg.gettlv(coreapi.CORE_TLV_CONF_VALUES)
+        if not values_str is None:
+            if not session in NetIDSubnetMap.__mapping__:
+                NetIDSubnetMap.__mapping__[session] = {}
+                NetIDSubnetMap.__mapping__[session][4] = {}
+                NetIDSubnetMap.__mapping__[session][6] = {}
+            # clear old map
+            else:
+                NetIDSubnetMap.__mapping__[session][cls.__ipversion__] = {}
+
+            values = values_str.split('|')
+            print(('[%s] values: %s' % (cls._name, str(values))))
+            idx = 0
+            while (idx+1) < len(values):
+                subnet = int(values[idx])
+                netid = int(values[idx+1])
+                print(('[%d] subnet: %d -- netid: %d' % (cls._name, subnet,
+                        netid)))
+                NetIDSubnetMap.__mapping__[session][cls.__ipversion__][subnet] = netid
+                idx += 2
+
+    @classmethod
+    def toconfmsg(cls, flags, nodenum, typeflags, values, session):
+        tlvdata = b''
+        tlvdata += coreapi.CoreConfTlv.pack(coreapi.CORE_TLV_CONF_OBJ,
+                cls._name)
+        tlvdata += coreapi.CoreConfTlv.pack(coreapi.CORE_TLV_CONF_TYPE,
+                typeflags)
+        datatypes = tuple(cls.__data_type__
+                for x in NetIDSubnetMap.__mapping__[session][cls.__ipversion__])
+        tlvdata += coreapi.CoreConfTlv.pack(coreapi.CORE_TLV_CONF_DATA_TYPES,
+                datatypes)
+        values_str = '|'.join(str(x) for x in d.values())
+        tlvdata += coreapi.CoreConfTlv.pack(coreapi.CORE_TLV_CONF_VALUES,
+                values_str)
+
+        msg = coreapi.CoreConfMessage.pack(flags, tlvdata)
+        return msg
+
+
+class NetIDSubnetMapIPv4(BaseNetIDSubnetMap):
+
+    _name = 'netid_subnet_map_ipv4'
+    __ipversion__ = 4
+
+class NetIDSubnetMapIPv6(BaseNetIDSubnetMap):
+
+    _name = 'netid_subnet_map_ipv6'
+    __ipversion__ = 6
+
